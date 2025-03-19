b57b6e0137921503f46b7391f973cb9398729d36
kishanpatel175 <sharedbykishan@gmail.com>
2025-03-19 20:13:32 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -4,13 +4,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyArCi2Sx0djkO3M7wD0X0JnkLbFh70GEOw",
-  authDomain: "has-ess-2.firebaseapp.com",
-  projectId: "has-ess-2",
-  storageBucket: "has-ess-2.firebasestorage.app",
-  messagingSenderId: "1041653137854",
-  appId: "1:1041653137854:web:f852018a38ec44ba6af9ef",
-  measurementId: "G-8ZMPSRNS3M"
+  apiKey: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  authDomain: "xxxxxxxxxx.firebaseapp.com",
+  projectId: "xxxxxxxxx",
+  storageBucket: "xxxxxxxxxxx.firebasestorage.app",
+  messagingSenderId: "xxxxxxxxxxxxxxxx",
+  appId: "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",
+  measurementId: "xxxxxxxxxxxxxx"
 };
 
 // Initialize Firebase
